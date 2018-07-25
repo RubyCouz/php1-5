2ee6b250fabae99a682ea746e84b4cd91e037121
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-25 16:15:55 +0200

    exo5

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 4</title>
+  </head>
+  <body>
+    <p>
+<?php
+ //$kill = null;
+ $kill = 26;
+ echo $kill;
+
+ ?>
+
+    </p>
+  </body>
+</html>
